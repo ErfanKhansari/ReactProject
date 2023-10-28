45c005b4532105536ec5677ac8034ec5f475132a
Khansari <ekhansari.749@gmail.com>
2023-10-28 23:48:37 +0330

    navbar changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,8 @@
 import styles from './Navbar.module.css'
 const Navbar = ()=>{
     return(
-        <nav className="navbar-fluid navbar-expand-lg bg-body-tertiary" >
-  <div className={`container-fluid ${styles.backG}`}>
+        <nav className="navbar-fluid navbar-expand-lg bg-body-tertiary sticky-top" >
+  <div className={`container ${styles.backG}`}>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
