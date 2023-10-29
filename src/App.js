693f19070042d1a0f9c60e4e13fa9643158ba4f8
Khansari <ekhansari.749@gmail.com>
2023-10-29 17:47:52 +0330

    app.js changed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import Cards from './components/Cards';
+import BackGround from './components/BackGround';
 
 function App() {
   return (
     <div >
       <Navbar />
+      <BackGround />
       <Cards />
     </div>
 
